aa6d2592a7ffcc78ee851cdb6a8f5c037212975f
wenner-sanner <wennersanner@hotmail.com>
2018-03-15 17:23:09 -0300

    Resolvendo o problema com arrow function

--- arrow-function/contexto-callback.js
+++ arrow-function/contexto-callback.js
@@ -4,12 +4,25 @@ const equipe = {
     nome: 'Guerreiros Z',
     membros: ['Goku', 'Kuririn', 'Vegeta'],
 
-    membrosDaEquipe: function() {
+    membrosDaEquipe1: function() {
         const that = this; //agora encherga o contexto do pai
         this.membros.forEach(function(membro) {
             console.log(`${membro} é da  equipe ${that.nome}`);
-        })
+        });
+    },
+
+    //resolvendo com arrow function. Foi projetada para capturar
+    //o this do seu contexto delimitador
+    membrosDaEquipe2: function() {
+        const that = this; //agora encherga o contexto do pai
+        this.membros.forEach((membro) => {
+            console.log(`${membro} é da  equipe ${that.nome}`);
+        });
     }
 }
 
-equipe.membrosDaEquipe();
+console.log('Sem arrow function:')
+equipe.membrosDaEquipe1();
+
+console.log('\nCom arrow function:')
+equipe.membrosDaEquipe2();
