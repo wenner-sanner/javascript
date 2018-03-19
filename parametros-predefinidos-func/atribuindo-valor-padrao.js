af466b84147f3d5307f9e4ff081d90d6032cd638
wenner-sanner <wennersanner@hotmail.com>
2018-03-19 19:43:48 -0300

    Atribuindo valores predefinidos aos parâmetros

---
+++ parametros-predefinidos-func/atribuindo-valor-padrao.js
@@ -0,0 +1,6 @@
+function nomeCompleto(nome, sobrenome, nomeDoMeio = '') {
+    console.log(`${nome} ${nomeDoMeio} ${sobrenome}`);
+}
+
+nomeCompleto('Wenner', 'Costa');
+nomeCompleto('Wenner', 'Costa', 'Garcia');
